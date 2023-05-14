df70cfa914fb0e29c0a50ab3438829bd5e54915f
ilyamus2201 <ilyamus11@gmail.com>
2023-05-14 16:30:43 +0700

    iwalk

---
+++ hello/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Love you!");
+            Console.ReadKey();
+        }
+    }
+}
